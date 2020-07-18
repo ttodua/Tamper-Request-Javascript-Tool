194193be04dd209b3d440c7767b968bd4d26d6e3
T. Todua <7117978+ttodua@users.noreply.github.com>
2020-07-18 22:15:54 +0400

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -1,22 +1,25 @@
-/*
+/* 
 * ###################################################################
 * ##### https://github.com/ttodua/Tamper-Request-Javascript-Tool ####
-* ### Don't put comments inside body when using the script inline ###
+* ### Don't put comments inside body when using the script inline ### 
 * ###################################################################
 */
 
 var AjaxMonitoringSample = 
 {
-	manualSumbission : false, 
-	allowedByAutoDisabling : true,
-	monitoring_status : true,
-	lastHeaders : {},
-	mainDivId : "AjaxMonitoringSample",
+	manualSumbission		: false,
+	monitoring_status 		: true,
+	responseOutput_status	: true,
+	lastHeaders 			: {},
+	allowed_requests		: [],
+	mainDivId				: "AjaxMonitoringSample",
 	
 	init(){
 		this.createDiv();
 		this.initXHR();
 	},
+	
+	resetAllowedRequests(){	 this.allowed_requests = [];	},
 	
 	createDiv(){
 		var div= document.createElement("div");
@@ -27,7 +30,7 @@ var AjaxMonitoringSample =
 			<textarea id="`+this.mainDivId+`_params" style="width:100%; height:40px;" placeholder="{key1:&quot;value1&quot;}"></textarea>
 			<textarea id="`+this.mainDivId+`_headers" style="width:100%; height:40px;" placeholder="{headersArray}"></textarea>
 			<input type="text" value="" id="`+this.mainDivId+`_url" style="width:100%;" placeholder="http://example.com" /> 
-			<button onclick="`+this.mainDivId+`.submitClicked();">resend</button>  <button id="`+this.mainDivId+`_status" onclick="`+this.mainDivId+`.toggleMonitoring();">ON/OFF</button> <input type="checkbox" id="`+this.mainDivId+`_autodisable" /> Disable automatically after first request (to avoid concurrent calls)
+			<button onclick="`+this.mainDivId+`.submitClicked();">resend</button>  <button id="`+this.mainDivId+`_status" onclick="`+this.mainDivId+`.toggleMonitoring(null);">ON/OFF</button> <input type="checkbox" id="`+this.mainDivId+`_autodisable" /> Disable automatically after first request (to avoid concurrent calls)
 			<div style="display:flex;height:40px;">Response: <textarea disabled id="`+this.mainDivId+`_response" style="width:100%; height:100%;"></textarea></div>
 		</div>`;
 		document.body.appendChild(div);
@@ -40,16 +43,17 @@ var AjaxMonitoringSample =
 		{ 
 			AjaxMonitoring_notfired=true;
  
+			
+			/* NOTE: XMLHttpRequest actions happen in this sequence: at first "open"[readyState=1] happens, then "setRequestHeader", then "send", then "open"[readyState=2] */
+
 			var XHR_SendOriginal = XMLHttpRequest.prototype.send;
 			XMLHttpRequest.prototype.send = function(data){
+ 
 				if (! this_.manualSumbission) 
 				{
-					if (this_.allowedByAutoDisabling) 
+					if (this_.monitoring_status)
 					{
-						if (this_.monitoring_status)
-						{
-							document.getElementById(this_.mainDivId+"_params").value = data; 
-						}
+						document.getElementById(this_.mainDivId+"_params").value = data; 
 					}
 				}
 			    XHR_SendOriginal.apply(this, arguments);
@@ -57,27 +61,36 @@ var AjaxMonitoringSample =
 
 			var XHR_OpenOriginal = XMLHttpRequest.prototype.open;
 			XMLHttpRequest.prototype.open = function(method, uri, async, user, pass) {
+
 				if (! this_.manualSumbission)
 				{
-					if (this_.allowedByAutoDisabling) 
-					{
-						if (this_.monitoring_status) 
+					this.addEventListener("readystatechange", function(event) { 
+
+						if(this.readyState == 1)
 						{
-							this.addEventListener("readystatechange", function(event) {   
-								if(this.readyState == 1){
-									if (method.toLowerCase()!="post_get_or_whatever") {
-										document.getElementById(this_.mainDivId+"_url").value = uri;
-									}
+							event.target.uniqueID = Math.floor( (Math.random() * 99999999) + 1 );
+							if (this_.monitoring_status) 
+							{
+								if (method.toLowerCase()!="post_get_or_whatever") {
+									document.getElementById(this_.mainDivId+"_url").value = uri;
 								}
-								if(this.readyState == 4){
-									if(this_.ifDisableAfterFirstExecution()) 
-										this_.toggleMonitoring();
-									this_.setResponse(this.responseText);
-									document.getElementById(this_.mainDivId+"_headers").value = JSON.stringify(this_.lastHeaders);
+								
+								if(this_.shouldBeDisabledAfterFirstTrigger()) {
+									this_.allowed_requests.push(event.target.uniqueID);
+									this_.toggleMonitoring(false);
+									return;
 								}
-							}, false);
+								this_.allowed_requests.push(event.target.uniqueID);
+							}
+						} 
+						if(this.readyState == 4){
+							if ( "uniqueID" in event.target &&  this_.allowed_requests.includes(event.target.uniqueID) )
+							{ 
+								this_.setResponse(this.responseText);
+								document.getElementById(this_.mainDivId+"_headers").value = JSON.stringify(this_.lastHeaders);
+							}
 						}
-					}
+					}, false); 
 				}
 				XHR_OpenOriginal.apply(this, arguments);
 			};
@@ -86,12 +99,9 @@ var AjaxMonitoringSample =
 			XMLHttpRequest.prototype.setRequestHeader = function(a, b) {
 				if (! this_.manualSumbission)
 				{
-					if (this_.allowedByAutoDisabling) 
+					if (this_.monitoring_status) 
 					{
-						if (this_.monitoring_status) 
-						{
-							this_.lastHeaders[a]=b;
-						}
+						this_.lastHeaders[a]=b;
 					}
 				}
 				XHR_SetRequestHeaderOriginal.apply(this, arguments);
@@ -130,16 +140,20 @@ var AjaxMonitoringSample =
 		http.send(params);
 	},
 	
-	toggleMonitoring() { 
-		this.monitoring_status = !this.monitoring_status; 
+	toggleMonitoring(enable_disable) { 
+		this.monitoring_status = enable_disable != null? enable_disable : !this.monitoring_status;
+		this.statusColorize(); 
 		if(this.monitoring_status)
 			this.allowedByAutoDisabling=true;
-		this.statusColorize();
-	},
+	}, 
+	
 	setResponse(text){
 		document.getElementById(this.mainDivId+"_response").value=text;
 	},
-	ifDisableAfterFirstExecution(){ return document.getElementById(this.mainDivId+"_autodisable").checked; } 
+	shouldBeDisabledAfterFirstTrigger(){ 
+		this.shouldBeDisabledAfterResponse = document.getElementById(this.mainDivId+"_autodisable").checked;
+		return this.shouldBeDisabledAfterResponse; 
+	} 
 };
 
 AjaxMonitoringSample.init();
